f3a8bd6c3f0af9ccf60f4645f5b445a2da663d32
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-06-30 02:37:13 +0530

    update: audit push and improvements

---
+++ templates/gen_readme.sh
@@ -0,0 +1,12 @@
+GREEN='\033[0;32m'
+RESET='\033[0m'
+alias greentick='echo -n ${GREEN}✔ ${RESET}'
+
+echo '# Ttile goes here ;)
+
+Author: [Abhijay Rajvansh](https://abhijayrajvansh.com)
+
+TBW' >> README.md
+
+greentick; echo 'sample readme template added to README.md'
+
