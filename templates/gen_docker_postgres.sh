10f85c4fd5cd2b2060bdf392066ab077766dec1b
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-07-13 06:29:59 +0530

    update: audit push and improvements

--- templates/gen_docker_postgres.sh
+++ templates/gen_docker_postgres.sh
@@ -2,17 +2,24 @@ GREEN='\033[0;32m'
 RESET='\033[0m'
 alias greentick='echo -n ${GREEN}✔ ${RESET}'
 
-echo 'services:
-  db:
-    image: postgres:latest
+echo '
+services: 
+  postgres:
+    image: postgres
     container_name: docker-postgres
     environment:
       POSTGRES_USER: postgres
-      POSTGRES_PASSWORD: postgres
       POSTGRES_DB: postgres
+      POSTGRES_PASSWORD: postgres
     ports:
-      - "5432:5432"
-' >> docker-compose.yaml
+      - 5432:5432
+    volumes:
+      - postgres_data:/var/lib/postgresql/data
+
+volumes:
+  postgres_data:
+' 
+>> docker-compose.yaml
 
 greentick; echo 'postgres service added to docker-compose.yaml.'
-echo 'DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres"'
+echo 'DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres?schema=public"'
